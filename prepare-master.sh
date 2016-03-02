0ae6c6f7181b241410282f30e4da993000934a77
matthewbyrne <matthewpbyrne@gmail.com>
2016-03-02 11:35:59 +0000

    Fixed change-directory bug-fix

--- prepare-master.sh
+++ prepare-master.sh
@@ -1,4 +1,4 @@
-export KUBE_SOURCE_DIR=${KUBE_SOURCE_DIR:-"~"}
+export KUBE_SOURCE_DIR=${KUBE_SOURCE_DIR:-"/home/ubuntu"}
 
 curl -O https://storage.googleapis.com/kubernetes-release/release/v1.1.8/bin/linux/amd64/kubectl
 chmod +x kubectl
--- run-master.sh
+++ run-master.sh
@@ -1,5 +1,5 @@
-export KUBE_SOURCE_DIR=${KUBE_SOURCE_DIR:-"~"}
-cd $KUBE_SOURCE_DIR/kubernetes
-KUBERNETES_PROVIDER=ubuntu ./cluster/kube-up.sh
-./cluster/ubuntu/deployAddons.sh
-
+export KUBE_SOURCE_DIR=${KUBE_SOURCE_DIR:-"/home/ubuntu"}
+cd $KUBE_SOURCE_DIR/kubernetes/cluster
+KUBERNETES_PROVIDER=ubuntu ./kube-up.sh
+cd ubuntu
+./deployAddons.sh
