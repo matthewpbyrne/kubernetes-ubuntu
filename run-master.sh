f6e4281b08ea81e2dc0cd9d0b7301f9d113e963f
matthewbyrne <matthewpbyrne@gmail.com>
2016-03-02 14:59:27 +0000

    Needed to qualify addons

--- run-master.sh
+++ run-master.sh
@@ -2,4 +2,4 @@ export KUBE_SOURCE_DIR=${KUBE_SOURCE_DIR:-"/home/ubuntu"}
 cd $KUBE_SOURCE_DIR/kubernetes/cluster
 KUBERNETES_PROVIDER=ubuntu ./kube-up.sh
 cd ubuntu
-./deployAddons.sh
+KUBERNETES_PROVIDER=ubuntu ./deployAddons.sh
