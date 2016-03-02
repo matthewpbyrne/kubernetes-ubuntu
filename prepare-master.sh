9e91b3e732e344d29590b2cebb17e1443f8986a4
matthewbyrne <matthewpbyrne@gmail.com>
2016-03-02 10:03:37 +0000

    Updated to install kubernetes in home directory

--- prepare-master.sh
+++ prepare-master.sh
@@ -1,4 +1,5 @@
 curl -O https://storage.googleapis.com/kubernetes-release/release/v1.1.8/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/kubectl
+cd ~
 git clone https://github.com/kubernetes/kubernetes.git
