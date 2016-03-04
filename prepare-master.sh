59d4f5780773fa5f23cefb04e5fa7292414eaa0c
matthewbyrne <matthewpbyrne@gmail.com>
2016-03-04 18:17:29 +0000

    Removed hardcoded /Users/matthewbyrne

--- prepare-master.sh
+++ prepare-master.sh
@@ -1,4 +1,4 @@
-export KUBE_SOURCE_DIR=${KUBE_SOURCE_DIR:-"/home/ubuntu"}
+export KUBE_SOURCE_DIR=${KUBE_SOURCE_DIR:-$HOME}
 
 curl -O https://storage.googleapis.com/kubernetes-release/release/v1.1.8/bin/linux/amd64/kubectl
 chmod +x kubectl
